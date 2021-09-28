bca489d1863fdfb72c3b6cdd8fc714bae8ebc4f5
Sharath Shetty <2019mc21012@wilp.bits-pilani.ac.in>
2021-09-29 05:29:58 +0530

    updated

--- src/main/java/com/sample/demo/DemoApplication.java
+++ src/main/java/com/sample/demo/DemoApplication.java
@@ -1,14 +1,9 @@
 package com.sample.demo;
 
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 // import org.springframework.core.env.PropertySource;
 
 @SpringBootApplication
@@ -22,8 +17,8 @@ import org.springframework.core.env.Environment;
 
 	public class DemoApplication  {
 
-		@Autowired
-		private Environment enviroment;
+		// @Autowired
+		// private Environment enviroment;
 		public static void main(String[] args) {
 			
 			// AnnotationConfigApplicationContext context =
@@ -46,20 +41,20 @@ import org.springframework.core.env.Environment;
 	// 	return application.sources(DemoApplication.class);
 	// }
 
-	private static void printSources (ConfigurableEnvironment env) {
-		System.out.println("-- property sources --");
-		for (org.springframework.core.env.PropertySource<?> propertySource : env.getPropertySources()) {
-			System.out.println("name =  " + propertySource.getName() + "\nsource = " + propertySource
-								.getSource().getClass()+"\n");
-		}
-	}
+	// private static void printSources (ConfigurableEnvironment env) {
+	// 	System.out.println("-- property sources --");
+	// 	for (org.springframework.core.env.PropertySource<?> propertySource : env.getPropertySources()) {
+	// 		System.out.println("name =  " + propertySource.getName() + "\nsource = " + propertySource
+	// 							.getSource().getClass()+"\n");
+	// 	}
+	// }
   
-	private static void printMap (Map<?, ?> map) {
-		map.entrySet()
-		   .stream().limit(15)
-		   .forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
-		System.out.println("-------------");
+	// private static void printMap (Map<?, ?> map) {
+	// 	map.entrySet()
+	// 	   .stream().limit(15)
+	// 	   .forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
+	// 	System.out.println("-------------");
   
-	}
+	// }
 
 }
